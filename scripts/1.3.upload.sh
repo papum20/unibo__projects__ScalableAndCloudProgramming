264d864d82f6709a5b8478dc2c7269d1e509b0cf
papum <danieledugo1@gmail.com>
2025-01-14 13:01:00 +0100

    scripts work to run locally. Now starting project

--- scripts/1.3.upload.sh
+++ scripts/1.3.upload.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [[ ! -d scripts ]]; then
+	echo "[ERROR] Scripts must be run from the project's root folder."
+	exit 1
+fi
+
 source ./0.1.variables.sh
 
-gcloud storage cp $PATH_SRC_JAR gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_JAR}
+gcloud storage cp "$PATH_SRC_JAR" "gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_JAR}"
 #gcloud storage cp $PATH_SRC_DATASET gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_DATASET}
