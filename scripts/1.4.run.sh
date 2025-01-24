2c5b8564e5ea4bfacb6edc879f663ede2899ccf0
papum <danieledugo1@gmail.com>
2025-01-24 16:36:00 +0100

    tests w/ attributes & params; trying to accept google fs, removed hdfs; testing

--- scripts/1.4.run.sh
+++ scripts/1.4.run.sh
@@ -10,5 +10,8 @@ source ./scripts/0.1.variables.sh
 gcloud dataproc jobs submit spark --cluster="$DATAPROC_CLUSTER_NAME" \
 	--bucket="${DATAPROC_BUCKET_NAME}" \
     --region="${DATAPROC_CLUSTER_REGION}" \
+    --driver-log-levels org.apache.spark=INFO,com.google.cloud.hadoop.gcsio=INFO \
+    --properties=spark.executor.memory=4g,spark.executor.cores=2,spark.driver.memory=4g,spark.sql.shuffle.partitions=50 \
     --jar="gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_JAR}" \
-    -- false "${DATAPROC_BUCKET_NAME}"
+    -- "gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_DATASET}" "gs://${DATAPROC_BUCKET_NAME}/out_main3"
+    #-- false "${DATAPROC_BUCKET_NAME}"
