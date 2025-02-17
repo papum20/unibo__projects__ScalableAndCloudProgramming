28e6780cacb4e4ae3103e5229ced491fa9dbc283
papum_debian <danieledugo1@gmail.com>
2025-02-17 13:38:53 +0100

    all tests except 3

--- scripts/1.4.run.sh
+++ scripts/1.4.run.sh
@@ -7,6 +7,9 @@ fi
 
 source ./scripts/0.1.variables.sh
 
+gcloud dataproc clusters start "$DATAPROC_CLUSTER_NAME" \
+    --region="$DATAPROC_CLUSTER_REGION"
+
 # remove recursively
 gcloud storage rm "gs://${DATAPROC_BUCKET_NAME}/${PATH_REMOTE_OUT}/**"
 
@@ -14,7 +17,7 @@ gcloud dataproc jobs submit spark --cluster="$DATAPROC_CLUSTER_NAME" \
 	--bucket="${DATAPROC_BUCKET_NAME}" \
     --region="${DATAPROC_CLUSTER_REGION}" \
     --jar="gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_JAR}" \
-    -- 4 false "${DATAPROC_BUCKET_NAME}" "${PATH_REMOTE_OUT}"
+    -- 3 false "${DATAPROC_BUCKET_NAME}" "${PATH_REMOTE_OUT}"
     #--driver-log-levels org.apache.spark=INFO,com.google.cloud.hadoop.gcsio=INFO \
     #--properties=spark.executor.memory=4g,spark.executor.cores=2,spark.driver.memory=4g,spark.sql.shuffle.partitions=50 \
     #-- "gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_DATASET}" "gs://${DATAPROC_BUCKET_NAME}/${PATH_REMOTE_OUT}"
