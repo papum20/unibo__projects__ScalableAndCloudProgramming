89f1366ed4b7153276a1c92338a3067ac2ed84ff
papum <danieledugo1@gmail.com>
2025-02-17 11:53:29 +0100

    doing tests

--- scripts/1.2.create-dataproc-cluster.sh
+++ scripts/1.2.create-dataproc-cluster.sh
@@ -7,17 +7,28 @@ fi
 
 source ./scripts/0.1.variables.sh
 
-gcloud dataproc clusters create "$DATAPROC_CLUSTER_NAME" \
-    --region="$DATAPROC_CLUSTER_REGION" \
-    --zone="$DATAPROC_CLUSTER_ZONE" \
-	--bucket="$DATAPROC_BUCKET_NAME" \
-	--temp-bucket="$DATAPROC_TMP_BUCKET_NAME" \
-	--num-workers "$DATAPROC_CLUSTER_NUM_WORKERS" \
-	--master-boot-disk-size "$DATAPROC_CLUSTER_MASTER_BOOT_DISK_SIZE" \
-	--worker-boot-disk-size "$DATAPROC_CLUSTER_WORKER_BOOT_DISK_SIZE" \
-	--master-machine-type="$DATAPROC_CLUSTER_MASTER_MACHINE_TYPE" \
-	--worker-machine-type="$DATAPROC_CLUSTER_WORKER_MACHINE_TYPE"
-	#--scopes storage-rw
-	#--optional-components=JUPYTER \
-	#--enable-component-gateway \
+if [[ $DATAPROC_CLUSTER_NUM_WORKERS == 1 ]] ; then
+	gcloud dataproc clusters create "$DATAPROC_CLUSTER_NAME" \
+		--region="$DATAPROC_CLUSTER_REGION" \
+		--zone="$DATAPROC_CLUSTER_ZONE" \
+		--bucket="$DATAPROC_BUCKET_NAME" \
+		--temp-bucket="$DATAPROC_TMP_BUCKET_NAME" \
+		--single-node \
+		--master-boot-disk-size "$DATAPROC_CLUSTER_MASTER_BOOT_DISK_SIZE" \
+		--master-machine-type="$DATAPROC_CLUSTER_MASTER_MACHINE_TYPE"
+else
+	gcloud dataproc clusters create "$DATAPROC_CLUSTER_NAME" \
+		--region="$DATAPROC_CLUSTER_REGION" \
+		--zone="$DATAPROC_CLUSTER_ZONE" \
+		--bucket="$DATAPROC_BUCKET_NAME" \
+		--temp-bucket="$DATAPROC_TMP_BUCKET_NAME" \
+		--num-workers "$DATAPROC_CLUSTER_NUM_WORKERS" \
+		--master-boot-disk-size "$DATAPROC_CLUSTER_MASTER_BOOT_DISK_SIZE" \
+		--worker-boot-disk-size "$DATAPROC_CLUSTER_WORKER_BOOT_DISK_SIZE" \
+		--master-machine-type="$DATAPROC_CLUSTER_MASTER_MACHINE_TYPE" \
+		--worker-machine-type="$DATAPROC_CLUSTER_WORKER_MACHINE_TYPE"
+		#--scopes storage-rw
+		#--optional-components=JUPYTER \
+		#--enable-component-gateway \
+fi
 
