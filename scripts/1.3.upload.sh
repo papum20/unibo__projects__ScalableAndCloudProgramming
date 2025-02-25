cae9ee0411cc2709d918234ae0b2b029846fd415
papum <danieledugo1@gmail.com>
2025-02-25 18:26:51 +0100

    submit

--- scripts/1.3.upload.sh
+++ scripts/1.3.upload.sh
@@ -11,4 +11,4 @@ gcloud dataproc clusters start "$DATAPROC_CLUSTER_NAME" \
     --region="$DATAPROC_CLUSTER_REGION"
 
 gcloud storage cp "$PATH_SRC_JAR" "gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_JAR}"
-#gcloud storage cp "$PATH_SRC_DATASET" "gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_DATASET}"
+gcloud storage cp "$PATH_SRC_DATASET" "gs://${DATAPROC_BUCKET_NAME}/${PATH_DST_DATASET}"
